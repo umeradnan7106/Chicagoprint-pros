be2eebecd124a90bb27a9eee18e31d351f0eb9f2
umeradnan7106 <120960218+umeradnan7106@users.noreply.github.com>
2025-10-16 12:27:10 +0500

    first commit

--- app/routes/auth.$.tsx
+++ app/routes/auth.$.tsx
@@ -1,14 +1,7 @@
-import type { LoaderFunctionArgs } from "@remix-run/node";
-import { authenticate } from "../shopify.server";
-import { registerWebhooks } from "../utils/register-webhooks.server";
-
-export const loader = async ({ request }: LoaderFunctionArgs) => {
-  const { admin } = await authenticate.admin(request); // GraphQL client only
-
-  await registerWebhooks(admin);
-
-  return new Response(null, {
-    status: 302,
-    headers: { Location: "/app" },
-  });
+// app/routes/auth.$.tsx 
+import type { LoaderFunctionArgs } from "@remix-run/node"; 
+import { authenticate } from "../shopify.server"; 
+export const loader = async ({ request }: LoaderFunctionArgs) => { 
+  await authenticate.admin(request); 
+  return null; 
 };

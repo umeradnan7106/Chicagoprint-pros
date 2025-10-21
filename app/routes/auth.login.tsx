be3d8667741bab6f8ffdb0bdb71dcdfc5c6522c7
umeradnan7106 <120960218+umeradnan7106@users.noreply.github.com>
2025-10-21 23:45:54 +0500

    eight commit

---
+++ app/routes/auth.login.tsx
@@ -0,0 +1,7 @@
+import type { LoaderFunctionArgs } from "@remix-run/node";
+import { login } from "../shopify.server";
+
+// This handles /auth/login redirects
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  return login(request);
+};
